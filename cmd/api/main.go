ce89adb8c310d2ebce89ed8fb080c5d994b6ee5d
Mlcarvalho1 <manoelliradecarvalho@gmail.com>
2026-02-04 15:28:25 -0300

    chore: clean git cache and respect .gitignore

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -16,26 +16,11 @@ func init() {
 	}
 }
 
-const banner = `
-       ,_---~~~~~----._
-_,,_,*^____      _____''*g*\"*,          Welcome to your app!
-/ __/ /'     ^.  /      \ ^@q   f     /
-[  @f | @))    |  | @))   l  0 _/    /
-\'/   \~____ / __ \_____/    \      /
- |           _l__l_           I     /
- }          [______]           I   /
- ]            | | |            |
- ]             ~ ~             |
- |                            |
-  |                           |`
-
 func main() {
 	app := fiber.New()
 	database.ConnectDb()
 
 	routes.SetupRoutes(app)
-
-	log.Println(banner)
 
 	if err := app.Listen(":3000"); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
